d77f57d61a2f7e3d43b81603c0e362a6e87c5b60
WilfredoRTG <wilfredo.tablerogz@udlap.mx>
2022-06-21 23:55:55 -0500

    Estructura de react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Home from "./screens/home/Home";
+import Other from "./screens/other/Other";
 
 function App() {
 	return (
@@ -8,6 +9,7 @@ function App() {
             <div>
                 <Switch>
                     <Route exact path="/" component={Home} />
+                    <Route exact path="/other" component={Other} />
                 </Switch>
             </div>
         </BrowserRouter>
---
+++ client/src/screens/other/Other.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Other = () => {
+  return (
+    <div>Other Route</div>
+  )
+}
+
+export default Other
