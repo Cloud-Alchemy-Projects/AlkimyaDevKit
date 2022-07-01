50c1e2e46f226b62f146fb9b4d2a66a73e6d845f
WilfredoRTG <wilfredo.tablerogz@udlap.mx>
2022-06-30 22:31:41 -0500

    New configuration with mysql 8 oracle

--- server/connection/sqlConnection.js
+++ server/connection/sqlConnection.js
@@ -7,7 +7,7 @@ export const db = await mysql2.createConnection(
     {
         host: process.env.MYSQL_HOST, 
         user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_ROOT_PASSWORD,
+        password: process.env.MYSQL_PASSWORD,
         database: process.env.MYSQL_DATABASE
     }
 );
