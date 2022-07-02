84dc74b7dbed35ae9700bf3d5894f5c8767c76f4
WilfredoRTG <wilfredo.tablerogz@udlap.mx>
2022-07-02 15:34:45 -0500

    Changes on the home route

---
+++ client/src/screens/home/styles.js
@@ -0,0 +1,12 @@
+import {makeStyles} from "@mui/styles"
+
+export default makeStyles(() => ({
+    mainContainer: {
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+}));
