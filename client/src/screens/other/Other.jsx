cf44848b3b2ffb0b0758231a2c2b7ec0e77dca9d
WilfredoRTG <wilfredo.tablerogz@udlap.mx>
2022-07-05 12:12:56 -0500

    Redux Dispatch completed

--- client/src/screens/other/Other.jsx
+++ client/src/screens/other/Other.jsx
@@ -1,9 +1,25 @@
-import {React} from 'react'
+import {React, useEffect} from 'react'
 import useStyles from "./styles"
 import FormOther from './components/FormOther';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUsers } from '../../actions/users';
+import { getProjects } from '../../actions/projects';
 
 const Other = () => {
     const classes = useStyles();
+
+    const dispatch = useDispatch();
+    const users = useSelector(state => state.users);
+    const projects = useSelector(state => state.projects);
+    
+    useEffect(() => {
+        dispatch(getUsers())
+        dispatch(getProjects())
+    }, [dispatch]);
+
+    console.log(users);
+    console.log(projects);
+
     return (
         <div className={classes.mainContainer}>
             <FormOther/>
