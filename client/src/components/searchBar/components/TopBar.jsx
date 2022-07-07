4c609f342bdb9fd3ecf74237765e8d8fe7a4573e
WilfredoRTG <wilfredo.tablerogz@udlap.mx>
2022-07-07 09:55:24 -0500

    Search Textfield edited

--- client/src/components/searchBar/components/TopBar.jsx
+++ client/src/components/searchBar/components/TopBar.jsx
@@ -1,27 +1,50 @@
-import React from 'react'
-import {TextField, Button} from '@mui/material'
-import useStyles from '../styles'
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import React from "react";
+import { TextField, Button, InputAdornment } from "@mui/material";
+import useStyles from "../styles";
+import { AddCircleOutline, Search, AccountCircle } from "@mui/icons-material";
 
 const TopBar = () => {
-    const classes = useStyles()
+	const classes = useStyles();
 
-    return (
-        <>
-            <div className={classes.appBar}>
-                <div className={classes.rightContainer}>
-                    <div className={classes.textToRight}>
-                        <h1 className={classes.h1bar}>Alkimya <strong className={classes.toPink}>Dev</strong>Kit</h1>
-                        <h5 className={classes.h5Bar}>by Cloud Alchemy</h5>
-                    </div>
-                </div>
-                <div className={classes.leftContainer}>
-                    <Button sx={{marginRight: '20px'}}variant="contained" startIcon={<AddCircleOutlineIcon/>}>New Project</Button>
-                    <TextField variant="outlined"></TextField>
-                </div>
-            </div>
-        </>
-    )
-}
+	return (
+		<>
+			<div className={classes.appBar}>
+				<div className={classes.rightContainer}>
+					<div className={classes.textToRight}>
+						<h1 className={classes.h1bar}>
+							Alkimya{" "}
+							<strong className={classes.toPink}>Dev</strong>Kit
+						</h1>
+						<h5 className={classes.h5Bar}>by Cloud Alchemy</h5>
+					</div>
+				</div>
+				<div className={classes.leftContainer}>
+					<Button
+						sx={{ marginRight: "20px" }}
+						variant="contained"
+						startIcon={<AddCircleOutline />}
+					>
+						New Project
+					</Button>
+					<TextField     
+						id="search-textfield"
+                        size="small"
+                        color="primary"
+						placeholder="Search..."
+						InputProps={{
+							startAdornment: (
+								<InputAdornment position="start">
+									<Search color="primary"/>
+								</InputAdornment>
+							),
+						}}
+						variant="outlined"
+                        focused
+					/>
+				</div>
+			</div>
+		</>
+	);
+};
 
-export default TopBar
+export default TopBar;
