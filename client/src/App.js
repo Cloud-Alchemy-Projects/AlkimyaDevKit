b41ea656b27d9b57f99cbaac510ff983c25e2230
IssacOM14 <issac.ortizmedel@gmail.com>
2022-07-08 13:13:04 -0500

    Cambios listos

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
-import styled from "styled-components";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Home from "./screens/home/Home";
 import Other from "./screens/other/Other";
-import SearchBar from "./components/searchBar/SearchBar";
+//import SearchBar from "./components/searchBar/SearchBar";
 
 function App() {
 	return (
 		<>
 			<BrowserRouter>
 				<div>
-                    <SearchBar/>
+                    {/* <SearchBar/> */}
 					<Switch>
 						<Route exact path="/" component={Home} />
 						<Route exact path="/other" component={Other} />
