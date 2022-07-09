2c4a5c08e7c50cef062feb9c4b016695ada56696
IssacOM14 <issac.ortizmedel@gmail.com>
2022-07-09 15:38:14 -0500

    Barra de herramientas

---
+++ client/src/components/toolBar/ToolsBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import useStyles from './styles'
+import LeftBar from './components/LeftBar'
+
+const ToolsBar = () => {
+ 
+    const classes = useStyles()
+
+    return(
+        <>
+            <LeftBar/>
+        </>
+    )
+}
+
+export default ToolsBar
