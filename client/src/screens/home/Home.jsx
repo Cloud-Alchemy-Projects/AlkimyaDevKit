0c6f1371dedb5b853044d5ae5d6ccdcc04408aaf
WilfredoRTG <wilfredo.tablerogz@udlap.mx>
2022-07-11 02:47:40 -0500

    Home page files added

--- client/src/screens/home/Home.jsx
+++ client/src/screens/home/Home.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
-import useStyles from './styles'
-import Login from './Components/Login'
-
+import {React, useEffect} from 'react'
+import useStyles from "./styles"
+// import FormOther from './components/Home';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { getUsers } from '../../actions/users';
+// import { getProjects } from '../../actions/projects';
+import ToolsBar from "../../components/toolBar/ToolsBar"
+import SearchBar from "../../components/searchBar/SearchBar"
+import { Typography } from '@mui/material';
 const Home = () => {
- 
-    const classes = useStyles()
+    const classes = useStyles();
 
-    return(
-        <>
-            <Login/>
-        </>
+    // const dispatch = useDispatch();
+    // const users = useSelector(state => state.users);
+    // const projects = useSelector(state => state.projects);
+    
+    // useEffect(() => {
+    //     dispatch(getUsers())
+    //     dispatch(getProjects())
+    // }, [dispatch]);
+
+    // console.log(users);
+    // console.log(projects);
+
+    return (
+        <div className={classes.mainContainer}>
+            <ToolsBar user="User"/>
+            <SearchBar color="white"/>
+            <div className={classes.homeContainer}>
+                <div className={classes.typoContainer}>
+                    <Typography className={classes.homeTypo} variant="h2">Timeless Innovation</Typography>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- client/src/screens/home/styles.js
+++ client/src/screens/home/styles.js
@@ -1,43 +1,30 @@
 import {makeStyles} from "@mui/styles"
+import fondo from "../../assets/images/background-home.jpg"
 
-import fondo from "../../assets/Images/fondo_1.jpg"
-
-
-export default makeStyles( theme => ({
-   
-    loginPage: {
-		width: '100%',
-		height: '100vh',
-		backgroundImage: `url(${fondo})`,
-		backgroundRepeat: 'no-repeat',
-		backgroundSize: 'cover',
-		backgroundPosition: 'center'
+export default makeStyles(() => ({
+    mainContainer: {
+        width: "100%",
+        height: "100vh",
+        backgroundImage: `url(${fondo})`,
+		backgroundRepeat: "no-repeat",
+		backgroundSize: "cover",
+		backgroundPosition: "center",
     },
-
-    logoIcon: { // <== Aqui se define el estilo del logo
-		transform: 'scale(0.09)',
-		marginLeft: '-440px',
-		marginTop: '-450px'
-	},
-
-    mainContainer:{
-		width: '400px', 
-		height: '885px', 
-		display: 'flex', 
-		flexDirection: 'column', 
-		borderRadius: '20px',
-        position: 'fixed', 
-		overflow: 'hidden', 
-		backgroundColor: '#E20074',
-		marginLeft: '1370px',
-		marginTop: '-490px'
+    homeContainer: {
+        width: '100%',
+        height: '100%',
+        paddingLeft: '500px'
     },
-
-    formContainer: {
+    typoContainer:{
+        width: '40%',
+        height: '100%',
         display: 'flex',
-        flexDirection: 'column',
-		marginTop: '110px',
-		marginLeft: '45px'
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    homeTypo: {
+        color: 'white',
+        fontWeight: '600 !important',
+        fontSize: '80px !important'
     }
-
 }));
