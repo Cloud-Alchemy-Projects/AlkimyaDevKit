107a43f6e9c87e3623c884016673ce547208b62d
IssacOM14 <issac.ortizmedel@gmail.com>
2022-07-10 21:52:24 -0500

    Cambios en la pagina login

--- client/src/screens/home/styles.js
+++ client/src/screens/home/styles.js
@@ -1,83 +1,43 @@
 import {makeStyles} from "@mui/styles"
 
-import fondo from "./Images/fondo_1.jpg"
+import fondo from "../../assets/Images/fondo_1.jpg"
 
 
 export default makeStyles( theme => ({
    
-    mainContainer: {
-        backgroundImage: `url(${fondo})`,
+    loginPage: {
+		width: '100%',
+		height: '100vh',
+		backgroundImage: `url(${fondo})`,
 		backgroundRepeat: 'no-repeat',
 		backgroundSize: 'cover',
-		backgroundPosition: 'center',
-		height: '100vh'
+		backgroundPosition: 'center'
     },
 
     logoIcon: { // <== Aqui se define el estilo del logo
-		transform: 'scale(0.032)',
-		marginLeft: '-1680px',
-		marginTop: '-1690px'
+		transform: 'scale(0.09)',
+		marginLeft: '-440px',
+		marginTop: '-450px'
 	},
 
-    mainTittle:{
-        color: '#fff',
-        fontWeight: '500',
-        fontSize: '3.8em',
-        lineHeight: '1.14',
-        zIndex: '10',
-        marginLeft: '150px',
-        marginTop: '240px',
-        textAlign: 'right',
-        letterSpacing: '2.2px'
+    mainContainer:{
+		width: '400px', 
+		height: '885px', 
+		display: 'flex', 
+		flexDirection: 'column', 
+		borderRadius: '20px',
+        position: 'fixed', 
+		overflow: 'hidden', 
+		backgroundColor: '#E20074',
+		marginLeft: '1370px',
+		marginTop: '-490px'
     },
 
-    mainTittle_02:{
-        color: '#fff',
-        fontWeight: '500',
-        fontSize: '5.8em',
-        lineHeight: '1.14',
-        zIndex: '10',
-        marginLeft: '-335px',
-        marginTop: '340px',
-        textAlign: 'right',
-        letterSpacing: '2.2px'
-    },
-
-    container: { // <== Este contenedor es el principal, el que contiene todos los elementos (Titulo, Subtitulo, Email, Password, Inputs y Button)
-		marginTop: theme.spacing(-5),
-		[theme.breakpoints.down(400 + theme.spacing(2) + 2)]: {
-			marginTop: 0,
-			width: '100%',
-			height: '100%'
-		}
-	},
-
-    div: { // <== Aqui se define la division de la pagina
-		marginTop: theme.spacing(4),
-		display: 'flex',
-		flexDirection: 'column',
-	},
-
-	form: { // <== Aqui se define el area del formato (Email y Password)
-		width: '100%',
-		marginTop: theme.spacing(15)
-	},
-
-    container_1: { // <== Este contenedor sera en que este por detras del TextField
-		opacity: '1',
-		height: '45px',
-		backgroundColor: 'white',
-		borderRadius: '100px', // <== Permite que tengamos bordes curvos del cuadro de texto
-		[theme.breakpoints.down(400 + theme.spacing(2) + 2)]: {
-			width: '100%',
-			height: '100%'
-		}
-	},
-
-    button: { // <== Aqui se define el boton de LOG IN
-		width: '40%'
-	},
-
-
+    formContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+		marginTop: '110px',
+		marginLeft: '45px'
+    }
 
 }));
