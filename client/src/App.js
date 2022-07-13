5dabab2be1daa7eb096fdd86ee3f87a1ea94453f
WilfredoRTG <wilfredo.tablerogz@udlap.mx>
2022-07-13 08:04:14 -0500

    Added the new route

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,17 @@
 import React from "react";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Login from "./screens/login/Login";
-//import Home from "./screens/home/Home";
+import Home from "./screens/home/Home";
 import Projects from "./screens/project/Projects";
-//import SearchBar from "./components/searchBar/SearchBar";
-//import ToolsBar from "./components/toolBar/ToolsBar";
 
 function App() {
 	return (
 		<>
 			<BrowserRouter>
 				<div>
-                    {/* <ToolsBar/>
-					<SearchBar/> */}
 					<Switch>
 						<Route exact path="/" component={Login} />
-						{/* <Route exact path="/home" component={Home} /> */}
+						<Route exact path="/home" component={Home} />
 						<Route exact path="/projects" component={Projects}/>
 					</Switch>
 				</div>
