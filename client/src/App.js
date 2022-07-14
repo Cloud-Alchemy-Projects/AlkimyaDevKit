3d7b207c1f4a7cd511f932574ac554aa96a06ff7
WilfredoRTG <wilfredo.tablerogz@udlap.mx>
2022-07-14 13:09:11 -0500

    Auth completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,24 @@
 import React from "react";
-import { Route, Switch, BrowserRouter } from "react-router-dom";
+import { Route, Switch, BrowserRouter, Router, Redirect } from "react-router-dom";
 import Login from "./screens/login/Login";
 import Home from "./screens/home/Home";
 import Projects from "./screens/project/Projects";
+import GuardRoute from "./helpers/GuardRoute";
+import { createBrowserHistory } from "history";
 
 function App() {
+    const history = createBrowserHistory();
 	return (
 		<>
-			<BrowserRouter>
-				<div>
-					<Switch>
-						<Route exact path="/" component={Login} />
-						<Route exact path="/home" component={Home} />
-						<Route exact path="/projects" component={Projects}/>
-					</Switch>
-				</div>
-			</BrowserRouter>
+			{/* <BrowserRouter> */}
+            <Router history={history}>
+                <Switch>
+                    <Route exact path="/" component={Login} />
+                    <GuardRoute exact path="/home" component={Home} />
+                    <GuardRoute exact path="/projects" component={Projects}/>
+                </Switch>
+            </Router>
+			{/* </BrowserRouter> */}
 		</>
 	);
 }
--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,8 +2,15 @@ import axios from 'axios';
 
 const API = axios.create({ baseURL: 'http://localhost:1010' });
 
+API.interceptors.request.use((req) =>{
+    if (localStorage.getItem('auth-token')) {
+        req.headers['auth-token'] = localStorage.getItem('auth-token')
+    }
+    return req;
+});
+
 // Login
-export const getUsers = () => API.get('users');
+export const signIn = (userData) => API.post('users/login', userData);
 
 // Projects
 export const getProjects = () => API.get('projects');
