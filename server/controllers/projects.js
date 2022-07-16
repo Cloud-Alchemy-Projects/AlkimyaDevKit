cca719da1707b67303b156d6af66300b5eef9da9
IssacOM14 <issac.ortizmedel@gmail.com>
2022-07-16 12:21:32 -0500

    Insercion New Projects

--- server/controllers/projects.js
+++ server/controllers/projects.js
@@ -8,3 +8,17 @@ export const sendProjects = async(req, res) =>{
         res.status(404).json({message: error.message})
     }
 };
+
+export const newProject = async(req, res) =>{
+
+    try {
+        await db.query(
+            'INSERT INTO proyectos (NombreProyecto,Estatus,Cliente,Usuario) VALUES \
+            (?,?,?,?)', 
+            [req.body.NombreProyecto, req.body.Estatus, req.body.Cliente,req.body.Usuario]
+            );
+        res.status(201).send(`project ${req.body.NombreProyecto} was successfully created`);
+    } catch (error) {
+        console.log(error.message)
+    }
+};
