237a6521736d9b11969745193fc7563cb7c2c406
WilfredoRTG <wilfredo.tablerogz@udlap.mx>
2022-07-20 01:16:58 -0500

    Add new endpoints for user stories

--- server/routes/userstories.js
+++ server/routes/userstories.js
@@ -1,12 +1,21 @@
 import express from "express";
-import {getUserStories, newUserStory, getRols, getEpics} from "../controllers/userstories.js"
+import {getUserStories, newUserStory, getRoles, getEpics, getRolesProject, newRoleProject, getEpicsProject, newEpicProject} from "../controllers/userstories.js"
 
 const router = express.Router()
 
+// Generals
 router.get("/all", getUserStories)
-router.get("/rols", getRols)
-router.get("/epics", getEpics)
 router.post("/new", newUserStory)
+
+// Rols
+router.get("/roles", getRoles)
+router.post("/roles/projects", getRolesProject)
+router.post("/roles/projects/new", newRoleProject)
+
+// Epics
+router.get("/epics", getEpics)
+router.post("/epics/projects", getEpicsProject)
+router.post("/epics/projects/new", newEpicProject)
 
 
 export default router;
