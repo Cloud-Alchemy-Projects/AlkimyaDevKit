c71b6e337ed596c94216859f91fd765c0dfad6ed
WilfredoRTG <wilfredo.tablerogz@udlap.mx>
2022-07-21 01:52:25 -0500

    Added the some of the new enpoints of the back-end

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -14,6 +14,15 @@ export const signIn = (userData) => API.post('login', userData);
 
 // Projects
 export const getProjects = () => API.get('projects');
+export const getProjectStatus = () => API.get('projects/status');
+export const createProject = (projectInfo) => API.post('projects/new', projectInfo);
 
 // User Stories
 export const getUserStories = () => API.get('userstories');
+
+// Clients
+export const getClients = () => API.get('clients/all')
+export const searchClient = (clientName) => API.post('clients/info/name', clientName)
+
+// Users
+export const getUsers = () => API.get('users/all')
