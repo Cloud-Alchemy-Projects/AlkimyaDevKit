e6a6fec7a0d62c1861810835b5c87028fb40ce73
WilfredoRTG <wilfredo.tablerogz@udlap.mx>
2022-07-21 01:52:49 -0500

    Added new reducer for client actions

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -4,10 +4,12 @@ import users from './users';
 import projects from './projects';
 import userstories from './userstories';
 import auth from './auth';
+import clients from './clients';
 
 export default combineReducers({
     users,
     projects,
     userstories,
+    clients,
     auth
 });
