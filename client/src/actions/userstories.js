4d0980fd9705d31df143e0ccd214c2b30bed550b
WilfredoRTG <wilfredo.tablerogz@udlap.mx>
2022-07-22 03:38:40 -0500

    Config of redux states

--- client/src/actions/userstories.js
+++ client/src/actions/userstories.js
@@ -1,10 +1,55 @@
-import {FETCH_ALL_US, CREATE_US, UPDATE_US} from "../constants/userstoriesActions.js"
+import {FETCH_ALL_US, CREATE_US, UPDATE_US, FETCH_ALL_ROLES, FETCH_ALL_EPICS, CREATE_EPIC, CREATE_ROLE} from "../constants/userstoriesActions.js"
 import * as api from '../api/index.js';
 
 export const getUserStories = () => async (dispatch) =>{
     try {
         const { data } = await api.getUserStories();
         dispatch({ type: FETCH_ALL_US, payload: data});
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const newUserStory = (userStoryData) => async (dispatch) =>{
+    try {
+        const { data } = await api.newUserStory(userStoryData);
+        dispatch({ type: CREATE_US, payload: data});
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const getRoles = () => async (dispatch) =>{
+    try {
+        const { data } = await api.allRoles();
+        dispatch({ type: FETCH_ALL_ROLES, payload: data});
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const newRole = (roleInfo) => async (dispatch) =>{
+    try {
+        const { data } = await api.newRole(roleInfo);
+        dispatch({ type: CREATE_ROLE, payload: data});
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const getEpics = () => async (dispatch) =>{
+    try {
+        const { data } = await api.allEpics();
+        dispatch({ type: FETCH_ALL_EPICS, payload: data});
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const newEpic = (epicInfo) => async (dispatch) =>{
+    try {
+        const { data } = await api.newEpic(epicInfo);
+        dispatch({ type: CREATE_EPIC, payload: data});
     } catch (error) {
         console.log(error);
     }
--- client/src/reducers/userstories.js
+++ client/src/reducers/userstories.js
@@ -1,14 +1,15 @@
-import {FETCH_ALL_US, CREATE_US, UPDATE_US} from "../constants/userstoriesActions.js"
+import {FETCH_ALL_US, CREATE_US, UPDATE_US, FETCH_ALL_ROLES, FETCH_ALL_EPICS} from "../constants/userstoriesActions.js"
 
 export default (userstories = [], action) => {
     switch (action.type) {
         case FETCH_ALL_US:
-            return{ ...userstories, fetchAll: {
-                ...userstories.fetchAll,
-                content: action.payload
-            }}  
+            return{ ...userstories, fetchAll: action.payload} 
         case CREATE_US:
-            return [...userstories, action.payload];
+            return{ ...userstories, create: [action.payload]}  
+        case FETCH_ALL_ROLES:
+            return{ ...userstories, fetchAllRoles: action.payload}  
+        case FETCH_ALL_EPICS:
+            return{ ...userstories, fetchAllEpics: action.payload}  
         default:
             return userstories;
     }
