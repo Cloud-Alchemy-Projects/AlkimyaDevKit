36e3eaf12c6c72e19ff93f67841ae794e86bfebf
WilfredoRTG <wilfredo.tablerogz@udlap.mx>
2022-07-24 21:10:10 -0500

    Log User state

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,13 +1,14 @@
 import {AUTH, LOGOUT} from "../constants/auth"
+import {LOG_USER} from "../constants/usersActions.js"
 import * as api from '../api/index.js';
 
 export const signin = (userData, router) => async (dispatch) => {
     try {
-        //console.log(formData);
         const { data } = await api.signIn(userData);
-        const token = data.data.token;
-        //console.log(data.data.token);
+        const token = data.token;
+        const logUser = data.logUser;
         dispatch({ type: AUTH, token });
+        dispatch({ type: LOG_USER, payload: logUser });
 
         router.push("/home");
     } catch (error) {
--- client/src/reducers/users.js
+++ client/src/reducers/users.js
@@ -1,11 +1,13 @@
-import {FETCH_ALL_USERS, CREATE_USERS, UPDATE_USERS} from "../constants/usersActions.js"
+import {FETCH_ALL_USERS, CREATE_USER, UPDATE_USERS, LOG_USER} from "../constants/usersActions.js"
 
 export default (users = [], action) => {
     switch (action.type) {
         case FETCH_ALL_USERS:    
-            return action.payload;
-        case CREATE_USERS:
+            return {...users, fetchAllusers: action.payload};
+        case CREATE_USER:
             return [...users, action.payload];
+        case LOG_USER:
+            return {...users, logUser: action.payload};
         default:
             return users;
     }
