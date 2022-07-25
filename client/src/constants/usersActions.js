cf619218c42edd8e53aa0e41170e7a03de411f09
WilfredoRTG <wilfredo.tablerogz@udlap.mx>
2022-07-24 21:12:57 -0500

    Added the log user action

--- client/src/constants/usersActions.js
+++ client/src/constants/usersActions.js
@@ -1,3 +1,4 @@
-export const CREATE_USERS = 'CREATE_USERS';
+export const CREATE_USER = 'CREATE_USER';
 export const UPDATE_USERS = 'UPDATE_USERS';
 export const FETCH_ALL_USERS = 'FETCH_ALL_USERS';
+export const LOG_USER = 'LOG_USER';
