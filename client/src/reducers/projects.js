d970329f5f2f5ae99b14d59e3bc0b361fb6b65f6
WilfredoRTG <wilfredo.tablerogz@udlap.mx>
2022-07-24 21:13:11 -0500

    Change the redux state of projects

--- client/src/reducers/projects.js
+++ client/src/reducers/projects.js
@@ -3,11 +3,11 @@ import {FETCH_ALL_PROJECT, CREATE_PROJECT, UPDATE_PROJECT, PROJECT_STATUS} from 
 export default (projects = [], action) => {
     switch (action.type) {
         case FETCH_ALL_PROJECT:    
-            return action.payload;
+            return { ...projects, fetchAllProjects: action.payload} 
         case PROJECT_STATUS:    
             return action.payload;
         case CREATE_PROJECT:
-            return [action.payload];
+            return { ...projects, createProject: [action.payload]} 
         default:
             return projects;
     }
