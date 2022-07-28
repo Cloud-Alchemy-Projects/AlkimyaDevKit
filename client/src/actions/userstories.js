6b33f9d6e6a41e38bf18cce8f9522157802f762f
IssacOM14 <issac.ortizmedel@gmail.com>
2022-07-28 12:15:29 -0500

    getUserStoriesStatus change at actions of UserStories

--- client/src/actions/userstories.js
+++ client/src/actions/userstories.js
@@ -1,10 +1,19 @@
-import {FETCH_ALL_US, CREATE_US, UPDATE_US, FETCH_ALL_ROLES, FETCH_ALL_EPICS, CREATE_EPIC, CREATE_ROLE} from "../constants/userstoriesActions.js"
+import {FETCH_ALL_US, CREATE_US, UPDATE_US, FETCH_ALL_ROLES, FETCH_ALL_EPICS, CREATE_EPIC, CREATE_ROLE, FETCH_ALL_STATUS} from "../constants/userstoriesActions.js"
 import * as api from '../api/index.js';
 
 export const getUserStories = () => async (dispatch) =>{
     try {
         const { data } = await api.getUserStories();
         dispatch({ type: FETCH_ALL_US, payload: data});
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const getUserStoriesStatus = () => async (dispatch) =>{ //GET USER STORIES STATUS USING REDUX
+    try {
+        const { data } = await api.getUserStoriesStatus();
+        dispatch({ type: FETCH_ALL_STATUS, payload: data});
     } catch (error) {
         console.log(error);
     }
